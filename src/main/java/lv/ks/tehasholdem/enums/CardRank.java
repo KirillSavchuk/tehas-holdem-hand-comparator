89bc498980976e9eb7908299a3e926f5bad6220f
Kirill Savchuk <kirillss1998@gmail.com>
2020-09-02 23:04:59 +0300

    Created Card Ranks and Suit enums

--- src/main/java/lv/ks/tehasholdem/enums/CardRank.java
+++ src/main/java/lv/ks/tehasholdem/enums/CardRank.java
@@ -1,14 +1,29 @@
 package lv.ks.tehasholdem.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum CardRank {
 
-	A('A', 1);
+	A('A', 14),
+	Q('Q', 13),
+	K('K', 12),
+	J('J', 11),
+	N10('T', 10),
+	N9('9', 9),
+	N8('8', 8),
+	N7('7', 7),
+	N6('6', 6),
+	N5('5', 5),
+	N4('4', 4),
+	N3('3', 3),
+	N2('2', 2);
 
-	public char title;
-	public byte weight;
+	private char title;
+	private byte weight;
 
-
-	CardRank(char a, int i) {
+	CardRank(char title, int weight) {
 	}
+
 }
 
--- src/main/java/lv/ks/tehasholdem/enums/CardSuit.java
+++ src/main/java/lv/ks/tehasholdem/enums/CardSuit.java
@@ -1,4 +1,18 @@
 package lv.ks.tehasholdem.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum CardSuit {
+
+	H('h'),
+	D('D'),
+	C('C'),
+	S('S');
+
+	private char suit;
+
+	CardSuit(char c) {
+	}
+
 }
