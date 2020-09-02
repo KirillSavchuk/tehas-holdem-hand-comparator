1a867b1babc01b80428a6d93648f17e7ee693e1f
Kirill Savchuk <kirillss1998@gmail.com>
2020-09-03 00:37:44 +0300

    Added Cart Rank and Suit static HashMap parsers

---
+++ src/main/java/lv/ks/tehasholdem/game/parser/CardRankParser.java
@@ -0,0 +1,38 @@
+package lv.ks.tehasholdem.game.parser;
+
+import lv.ks.tehasholdem.game.enums.CardRank;
+import org.springframework.stereotype.Service;
+
+import static lv.ks.tehasholdem.game.enums.CardRank.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class CardRankParser {
+
+	private static final Map<Character, CardRank> CARD_RANKS_MAP = new HashMap<>();
+
+	static {
+		CARD_RANKS_MAP.put('A', ACE);
+		CARD_RANKS_MAP.put('K', KING);
+		CARD_RANKS_MAP.put('Q', QUEEN);
+		CARD_RANKS_MAP.put('J', JACK);
+		CARD_RANKS_MAP.put('T', TEN);
+		CARD_RANKS_MAP.put('9', NINE);
+		CARD_RANKS_MAP.put('8', EIGHT);
+		CARD_RANKS_MAP.put('7', SEVEN);
+		CARD_RANKS_MAP.put('6', SEVEN);
+		CARD_RANKS_MAP.put('5', FIVE);
+		CARD_RANKS_MAP.put('4', FOUR);
+		CARD_RANKS_MAP.put('3', THREE);
+		CARD_RANKS_MAP.put('2', TWO);
+	}
+
+	public CardRank parseCardRank(Character cardRank) {
+		return Optional.ofNullable(CARD_RANKS_MAP.get(cardRank))
+			.orElseThrow(NullPointerException::new);
+	}
+
+}
---
+++ src/main/java/lv/ks/tehasholdem/game/parser/CardSuitParser.java
@@ -0,0 +1,30 @@
+package lv.ks.tehasholdem.game.parser;
+
+import lv.ks.tehasholdem.game.enums.CardRank;
+import lv.ks.tehasholdem.game.enums.CardSuit;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static lv.ks.tehasholdem.game.enums.CardSuit.*;
+
+@Service
+public class CardSuitParser {
+
+	private static final Map<Character, CardSuit> CARD_SUITS_MAP = new HashMap<>();
+
+	static {
+		CARD_SUITS_MAP.put('h', HEARTS);
+		CARD_SUITS_MAP.put('d', DIAMONDS);
+		CARD_SUITS_MAP.put('c', CLUBS);
+		CARD_SUITS_MAP.put('s', SPADES);
+	}
+
+	public CardSuit parseCardRank(Character cardSuit) {
+		return Optional.ofNullable(CARD_SUITS_MAP.get(cardSuit))
+			.orElseThrow(NullPointerException::new);
+	}
+
+}
